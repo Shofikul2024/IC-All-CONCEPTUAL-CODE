ec39b04cdfbcd018ca7e3d781ae8ff14c2584e12
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-08-07 23:59:06 +0600

    added  string

--- string_methods(conceptual_class).py
+++ string_methods(conceptual_class).py
@@ -8,3 +8,21 @@
 #
 # full_name="shamim   shofikul islam   "
 # print("upper:", full_name.upper())
+
+
+
+
+
+
+
+# user_name="SABBIR"
+# print("Lowercase:", user_name.lower())     #convert bentire string  to lowercase
+
+
+
+
+
+
+
+sentance ="i love python "
+print("uporer sentance ti ke capitalize koro orthad word er 1 st character capital koro:", sentance.capitalize())

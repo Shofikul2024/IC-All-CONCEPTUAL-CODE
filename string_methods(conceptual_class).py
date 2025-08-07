c87f64e1a203dadfd7357588de5f7464000636c1
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-08-08 01:08:49 +0600

    digit check

--- string_methods(conceptual_class).py
+++ string_methods(conceptual_class).py
@@ -44,8 +44,53 @@
 
 
 # ============================= Swap uppercase to lowercase and vice versa==================
+#
+#
+#
+# mixez_case_text="pyThoN"
+# print("Swapcase:", mixez_case_text.swapcase())
 
 
 
-mixez_case_text="pyThoN"
-print("Swapcase:", mixez_case_text.swapcase())
+
+
+
+
+# ============================================== Whitespace Handling and Validation     =========================================
+# ======================================================== Remove trailing whitespaces    =========================================
+
+
+# email_with_space="                      islamshofi942@gmail.com "
+# print("Right Stripped Email:" ,email_with_space.split())
+
+
+
+
+
+
+
+
+
+
+
+
+# ===========================================  Check if string contains only digits =========================================
+
+# ===========================================    Check if string contains only digits     =========================================
+
+#
+#
+# numeric_string="12354696897"
+# print("uporer number goli  digit kina check er method:", numeric_string.isdigit())
+
+
+
+# ===========================================         =========================================
+
+
+
+
+
+
+
+
